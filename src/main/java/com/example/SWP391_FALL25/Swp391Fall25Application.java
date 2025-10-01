9e8d5c69eb8f28b8674ad8378d36bf2ca37d1fb7
baothanh4 <taobaothanh365@gmail.com>
2025-10-01 08:14:34 +0700

    Update sql server configuration

---
+++ src/main/java/com/example/SWP391_FALL25/Swp391Fall25Application.java
@@ -0,0 +1,13 @@
+package com.example.SWP391_FALL25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swp391Fall25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swp391Fall25Application.class, args);
+	}
+
+}
