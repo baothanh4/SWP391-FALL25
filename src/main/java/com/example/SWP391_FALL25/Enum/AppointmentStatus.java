79e8913d0c26aa43eed45ac5c43099063056bf70
ThinhNP <151729430+ZidenVN@users.noreply.github.com>
2025-10-21 08:56:16 +0700

    Add Enum
    Add Enum PaymentStatus, Appointment Status

--- src/main/java/com/example/SWP391_FALL25/Enum/AppointmentStatus.java
+++ src/main/java/com/example/SWP391_FALL25/Enum/AppointmentStatus.java
@@ -3,7 +3,12 @@ package com.example.SWP391_FALL25.Enum;
 public enum AppointmentStatus {
     PENDING,
     ASSIGNED,
+    INSPECTING,
+    QUOTATION_SENT,
+    APPROVE,
     IN_PROGRESS,
     COMPLETED,
+    PAYMENT_PENDING,
+    PAID,
     CANCELLED
 }
---
+++ src/main/java/com/example/SWP391_FALL25/Enum/PaymentStatus.java
@@ -0,0 +1,11 @@
+package com.example.SWP391_FALL25.Enum;
+
+public enum PaymentStatus {
+    QUOTATION,         // Báo giá (chưa duyệt)
+    PENDING,           // Chờ thanh toán (đã duyệt)
+    PROCESSING,        // Đang xử lý thanh toán
+    COMPLETED,         // Đã thanh toán
+    FAILED,            // Thanh toán thất bại
+    REFUNDED,
+    CANCELED
+}
