ebc3dd027138e5c22fc057156164a44727f9e04b
baothanh4 <taobaothanh365@gmail.com>
2025-10-28 07:06:42 +0700

    fix add vehicle

--- src/main/java/com/example/SWP391_FALL25/Repository/VehicleRepository.java
+++ src/main/java/com/example/SWP391_FALL25/Repository/VehicleRepository.java
@@ -4,7 +4,10 @@ import com.example.SWP391_FALL25.Entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle,Long> {
+    Optional<Vehicle> findByVin(String vin);
 }
